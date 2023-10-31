7a51a290850c1d3c44d4857715797e3cf9e4f6de
Nasuha Asri <NASAS@orsted.com>
2023-10-31 14:44:41 +0800

    create category models

---
+++ BulkyBookWeb/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace BulkyBookWeb.Models;
+
+public class Category
+{
+    // create all properties
+
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int DisplayOrder { get; set; }
+    public DateTime CreatedDateTime { get; set; } = DateTime.Now; // put default value
+}
