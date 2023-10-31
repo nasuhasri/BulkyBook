b0c5cccc293b1b8a40d565ae94828350ae24d1c2
Nasuha Asri <NASAS@orsted.com>
2023-10-31 14:49:09 +0800

    add data annotations - to add validation to the property

--- BulkyBookWeb/Models/Category.cs
+++ BulkyBookWeb/Models/Category.cs
@@ -1,10 +1,16 @@
-﻿namespace BulkyBookWeb.Models;
+﻿// this will tell the EF to make the Id as a primary key and name is required in the database
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models;
 
 public class Category
 {
     // create all properties
 
+    // add data annotations
+    [Key]
     public int Id { get; set; }
+    [Required]
     public string Name { get; set; }
     public int DisplayOrder { get; set; }
     public DateTime CreatedDateTime { get; set; } = DateTime.Now; // put default value
