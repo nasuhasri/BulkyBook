8dd6d45b512da8867153d800ff0b1d5781dbcee8
Nasuha Asri <NASAS@orsted.com>
2023-11-21 10:50:13 +0800

    add connection string

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -1,4 +1,10 @@
 // program that responsible in running the application
+// contains code to start the server, listen for incoming requests, code to configure the application
+
+// CreateBuilder set up Kestrel web server, configure IIS integration, specify content root, read
+// application settings from appsettings.json
+// Kestrel is built-in cross platform server, run as part of our application and handle the requests
+// is placed behind a real web server like IIS where it is a windows server
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
