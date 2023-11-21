eac5ed789b0e175c11070e941d1fcc13d37caf6b
Nasuha Asri <NASAS@orsted.com>
2023-11-21 11:14:43 +0800

    add db.
    1. install efcore from nuget package solution.
    2. add ApplicationDbContext file to use EfCore.
    3. add DbSet to add table for our db

---
+++ BulkyBookWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using BulkyBookWeb.Models;
+using Microsoft.EntityFrameworkCore; // used for database, must install using nuget package
+
+namespace BulkyBookWeb.Data;
+
+/*
+ * there are 2 models; code-first approach and db-first approach. currently, we adopt code-first approach.
+ * bcs we pwriting code of the model first then create the db based on the model
+ */
+public class ApplicationDbContext : DbContext
+{
+    // receive options and pass it to the base class which is db context
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+    {
+    }
+
+    // create the category table with name as Categories and have four columns as written in category model
+    public DbSet<Category> Categories { get; set; }
+}
