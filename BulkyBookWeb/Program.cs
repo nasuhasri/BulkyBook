a7b3cc55dbaa2a152d707e7e1a1730c20a3bc7cb
Nasuha Asri <NASAS@orsted.com>
2023-11-22 15:20:57 +0800

    setup program.cs to use db context:
    - install efcore sqlserver
    - use options.UseSqlSercer to get connection string

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -5,10 +5,19 @@
 // application settings from appsettings.json
 // Kestrel is built-in cross platform server, run as part of our application and handle the requests
 // is placed behind a real web server like IIS where it is a windows server
+using BulkyBookWeb.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+// to add db context, must install efcore.sqlserver. version must be same with efcore.
+// ApplicationDbContext receive options, so we must pass our connection string to the options
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(
+    // it will extract the info of ConnectionString from appsettings.json
+    builder.Configuration.GetConnectionString("DefaultConnection")
+));
 
 var app = builder.Build();
 
