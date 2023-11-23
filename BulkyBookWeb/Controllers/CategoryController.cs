88c16cccc47b9dc87815448608a4af63da96b9be
Nasuha Asri <NASAS@orsted.com>
2023-11-23 18:37:31 +0800

    add delete functionality

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -93,13 +93,59 @@ public class CategoryController : Controller
         // server-side validation
         if (ModelState.IsValid)
         {
-            _db.Categories.Add(obj);
+            _db.Categories.Update(obj);
             _db.SaveChanges(); // this code will push the new record to the db
+
             return RedirectToAction("Index"); // redirect to action within the same controller
         }
 
         return View(obj);
 
         // RedirectToAction("Index", "<controller>") -> example of redirect to action in another controller
+    }
+
+    // GET
+    public IActionResult Delete(int? id)
+    {
+        if (id == null || id == 0)
+        {
+            return NotFound();
+        }
+
+        /*
+         * Single - throw an exception if no elements are found for that ID
+         * SingleOrDefault - return empty if there are no elements
+         * Default - will not throw an exception if element is not found
+         * 
+         * First - throw an exception if there are more than one elements
+         * FirstOrDefault - return the first elements of the list
+         */
+        // var categoryFromDb = _db.Categories.Find(id); // using find() method
+        var categoryFromDb = _db.Categories.FirstOrDefault(c => c.Id == id);
+
+        if (categoryFromDb == null)
+        {
+            return NotFound();
+        }
+
+        return View(categoryFromDb);
+    }
+
+    // POST
+    [HttpPost, ActionName("Delete")] // can also use Delete name inside the view page as action
+    [ValidateAntiForgeryToken] // prevent cross site request forgery attack
+    public IActionResult DeletePost(int? id)
+    {
+        var obj = _db.Categories.Find(id);
+
+        if (obj == null)
+        {
+            return NotFound();
+        }
+
+        _db.Categories.Remove(obj);
+        _db.SaveChanges();
+
+        return RedirectToAction("Index");
     }
 }
