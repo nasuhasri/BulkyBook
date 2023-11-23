fde185c2515399eb91ee9eceb7f24ef37683c2b3
Nasuha Asri <NASAS@orsted.com>
2023-11-23 17:49:09 +0800

    - add client-side validation using partial view - add annotation on the model in the category.cs file

--- BulkyBookWeb/Models/Category.cs
+++ BulkyBookWeb/Models/Category.cs
@@ -12,6 +12,8 @@ public class Category
     public int Id { get; set; }
     [Required]
     public string Name { get; set; }
+    [Display(Name = "Display Order")]
+    [Range(1, 100, ErrorMessage = "Display Order must be between 1 and 100 only!")]
     public int DisplayOrder { get; set; }
     public DateTime CreatedDateTime { get; set; } = DateTime.Now; // put default value
 }
