5b7f2c3aa0d4e51fb6c04406125b95a06208ca94
Nasuha Asri <NASAS@orsted.com>
2023-11-23 11:22:58 +0800

    add bootstrap theme which is bootswatch

--- BulkyBookWeb/Program.cs
+++ BulkyBookWeb/Program.cs
@@ -19,6 +19,8 @@ builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlSer
     builder.Configuration.GetConnectionString("DefaultConnection")
 ));
 
+builder.Services.AddRazorPages();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
